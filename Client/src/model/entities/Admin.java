ce7dcfedbdb10aed04a860c26fee5b6a856a6e86
Tutel <jas.lewek@gmail.com>
2025-04-25 19:49:35 +0200

    Made views for modify train
    Took 1 hour 24 minutes

---
+++ Client/src/model/entities/Admin.java
@@ -0,0 +1,72 @@
+package model.entities;
+
+public class Admin extends User
+{
+  private static final boolean isAdmin = true;
+
+  public Admin(String name, String password, String email, MyDate birthDate)
+  {
+    super(name, password, email, birthDate);
+  }
+
+  public String getName()
+  {
+    return super.getName();
+  }
+
+  public void setName(String name)
+  {
+    super.Name(name);
+  }
+
+  public String getPassword()
+  {
+    return super.getPassword();
+  }
+
+  public void setPassword(String password)
+  {
+    super.setPassword(password);
+  }
+
+  public String getEmail()
+  {
+    return super.getEmail();
+  }
+
+  public void setEmail(String email)
+  {
+    super.setEmail(email);
+  }
+
+  public MyDate getBirthDate()
+  {
+    return super.getBirthDate();
+  }
+
+  public void setBirthDate(MyDate birthDate)
+  {
+    super.setBirthDate(birthDate);
+  }
+
+  public String toString()
+  {
+    return "Admin " + super.toString();
+  }
+
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if (obj == null || getClass() != obj.getClass())
+      return false;
+
+    Admin admin = (Admin) obj;
+    return isAdmin == Admin.isAdmin && super.equals(obj);
+  }
+
+  public boolean isAdmin()
+  {
+    return Admin.isAdmin;
+  }
+}
