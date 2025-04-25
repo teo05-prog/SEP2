31e5cfa60e26c31220d442db3dd78439d8f9a861
Tutel <jas.lewek@gmail.com>
2025-04-25 19:49:35 +0200

    Traveller, Admin, Ticket, MyDate

--- Client/src/model/entities/MyDate.java
+++ Client/src/model/entities/MyDate.java
@@ -1,16 +1,162 @@
 package model.entities;
 
+import java.time.LocalDate;
+
 public class MyDate
 {
-    private int day;
-    private int month;
-    private int year;
+  private int day;
+  private int month;
+  private int year;
 
-    public MyDate(int day, int month, int year) {
-      this.day = day;
-      this.month = month;
-      this.year = year;
+  private int second;
+  private int minute;
+  private int hour;
+
+  public MyDate(int day, int month, int year, int hour, int minute, int second)
+  {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+    this.hour = hour;
+    this.minute = minute;
+    this.second = second;
+  }
+
+  public MyDate(int year, int month, int day)
+  {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  public MyDate(MyDate date)
+  {
+    this.day = date.day;
+    this.month = date.month;
+    this.year = date.year;
+    this.hour = date.hour;
+    this.minute = date.minute;
+    this.second = date.second;
+  }
+
+  public int getDay()
+  {
+    return day;
+  }
+
+  public void setDay(int day)
+  {
+    this.day = day;
+  }
+
+  public int getMonth()
+  {
+    return month;
+  }
+
+  public void setMonth(int month)
+  {
+    this.month = month;
+  }
+
+  public int getYear()
+  {
+    return year;
+  }
+
+  public void setYear(int year)
+  {
+    this.year = year;
+  }
+
+  public int getSecond()
+  {
+    return second;
+  }
+
+  public void setSecond(int second)
+  {
+    this.second = second;
+  }
+
+  public int getMinute()
+  {
+    return minute;
+  }
+
+  public void setMinute(int minute)
+  {
+    this.minute = minute;
+  }
+
+  public int getHour()
+  {
+    return hour;
+  }
+
+  public void setHour(int hour)
+  {
+    this.hour = hour;
+  }
+
+  public static MyDate today()
+  {
+    LocalDate currentDate = LocalDate.now();
+    int currentDay = currentDate.getDayOfMonth();
+    int currentMonth = currentDate.getMonthValue();
+    int currentYear = currentDate.getYear();
+
+    return new MyDate(currentYear, currentMonth, currentDay);
+  }
+
+  public boolean isBefore(MyDate other)
+  {
+    if (other.year < year)
+    {
+      return true;
     }
+    else if (other.month < month && other.year == year)
+    {
+      return true;
+    }
+    else if (other.day < day && other.month == month && other.year == year)
+    {
+      return true;
+    }
+    else if (other.hour < hour && other.day == day && other.month == month
+        && other.year == year)
+    {
+      return true;
+    }
+    else if (other.minute < minute && other.hour == hour && other.day == day
+        && other.month == month && other.year == year)
+    {
+      return true;
+    }
+    else if (other.second < second && other.minute == minute
+        && other.hour == hour && other.day == day && other.month == month
+        && other.year == year)
+    {
+      return true;
+    }
+    return false;
+  }
 
-    // getters/setters
+  public boolean equals(Object obj)
+  {
+    if (obj == null || getClass() != obj.getClass())
+    {
+      return false;
+    }
+    MyDate other = (MyDate) obj;
+
+    return day == other.day && month == other.month && year == other.year
+        && hour == other.hour && minute == other.minute
+        && second == other.second;
+  }
+
+  public MyDate copy()
+  {
+    return new MyDate(year, month, day, hour, minute, second);
+  }
 }
---
+++ Client/src/model/entities/Traveller.java
@@ -0,0 +1,82 @@
+package model.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Traveller extends User
+{
+  private static final boolean isAdmin = false;
+  private List<Ticket> tickets;
+
+  public Traveller(String name, String password, String email, MyDate birthDate)
+  {
+    super(name, password, email, birthDate);
+    tickets = new ArrayList<Ticket>();
+  }
+
+  public static boolean isAdmin()
+  {
+    return isAdmin;
+  }
+
+  public void buyTicket(Ticket ticket)
+  {
+    tickets.add(ticket);
+  }
+
+  public String getName()
+  {
+    return super.getName();
+  }
+
+  public void setName(String name)
+  {
+    super.Name(name);
+  }
+
+  public String getPassword()
+  {
+    return super.getPassword();
+  }
+
+  public void setPassword(String password)
+  {
+    super.setPassword(password);
+  }
+
+  public String getEmail()
+  {
+    return super.getEmail();
+  }
+
+  public void setEmail(String email)
+  {
+    super.setEmail(email);
+  }
+
+  public MyDate getBirthDate()
+  {
+    return super.getBirthDate();
+  }
+
+  public void setBirthDate(MyDate birthDate)
+  {
+    super.setBirthDate(birthDate);
+  }
+
+  public String toString()
+  {
+    return "Traveller " + super.toString();
+  }
+
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if (obj == null || getClass() != obj.getClass())
+      return false;
+
+    Traveller traveller = (Traveller) obj;
+    return isAdmin == Traveller.isAdmin && super.equals(obj);
+  }
+}
