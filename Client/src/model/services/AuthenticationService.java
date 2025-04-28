4b14e986186195c4e736494e5a82a3c1f1ab942d
bianca0203 <biancabuzdugan00@gmail.com>
2025-04-28 19:23:00 +0200

    Implement login functionality and validation in AuthenticationService and LogInVM
    Took 47 minutes

--- Client/src/model/services/AuthenticationService.java
+++ Client/src/model/services/AuthenticationService.java
@@ -4,7 +4,7 @@ import model.entities.User;
 
 public interface AuthenticationService
 {
-  //public abstract String login(LoginRequest request);
+  public abstract String login(LoginRequest request);
 
   public abstract String register(User user);
 }
