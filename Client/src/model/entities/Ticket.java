1f50885fa79d60a10e6b3436f51b113a0ec564d5
bianca0203 <biancabuzdugan00@gmail.com>
2025-04-30 21:19:53 +0200

    Add Ticket class to manage ticket entities with ID, type, and seat information
    Took 4 minutes

--- Client/src/model/entities/Ticket.java
+++ Client/src/model/entities/Ticket.java
@@ -2,5 +2,39 @@ package model.entities;
 
 public class Ticket
 {
-  public Ticket(){}
+  private int ticketID;
+  private String type;
+  private Seat seatId;
+  public Ticket(int ticketID, String type, Seat seatId)
+  {
+    this.ticketID = ticketID;
+    this.type = type;
+    this.seatId = seatId;
+
+  }
+  public int getTicketID()
+  {
+    return ticketID;
+  }
+  public void setTicketID(int ticketID)
+  {
+    this.ticketID = ticketID;
+  }
+  public String getType()
+  {
+    return type;
+  }
+  public void setType(String type)
+  {
+    this.type = type;
+  }
+  public Seat getSeatId()
+  {
+    return seatId;
+  }
+  public String toString()
+  {
+    return "Ticket ID: " + ticketID + ", Type: " + type + ", Seat ID: " + seatId.getSeatId();
+  }
+
 }
