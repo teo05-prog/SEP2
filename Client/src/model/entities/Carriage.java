97545ae59456456004f24fc8ab5fe9b571e86882
bianca0203 <biancabuzdugan00@gmail.com>
2025-04-30 20:20:01 +0200

    Refactor Carriage class to manage seats with add and remove methods
    Took 20 minutes

--- Client/src/model/entities/Carriage.java
+++ Client/src/model/entities/Carriage.java
@@ -1,29 +1,40 @@
 package model.entities;
 
+import java.util.ArrayList;
+
 public class Carriage
 {
     private int carriageId;
-    private final int seat =16;
-    private final int bicycle=2;
+    private final int seatCount = 16;
+    private ArrayList<Seat> seats;
 
     public Carriage(int carriageId) {
         this.carriageId = carriageId;
+        this.seats = new ArrayList<>(seatCount);
     }
-
     public int getCarriageId() {
         return carriageId;
     }
-
     public void setCarriageId(int carriageId) {
         this.carriageId = carriageId;
     }
-
-    public int getSeat() {
-        return seat;
+    public void addSeat(Seat seat) {
+        this.seats.add(seat);
+    }
+    public ArrayList<Seat> getSeats() {
+        return seats;
+    }
+    public void removeSeat(Seat seat) {
+        this.seats.remove(seat);
+    }
+    public void removeSeatById(int seatId) {
+        for (int i = 0; i < seats.size(); i++) {
+            if (seats.get(i).getSeatId() == seatId) {
+                seats.remove(i);
+            }
+        }
     }
 
-    public int getBicycle() {
-        return bicycle;
-    }
+
 
 }
