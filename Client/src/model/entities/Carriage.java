30a23533a2de0478ccf0a420a6559db582c87280
bianca0203 <biancabuzdugan00@gmail.com>
2025-04-30 21:15:52 +0200

    Add bicycle management to Carriage class with add, remove, and retrieval methods
    Took 22 minutes

--- Client/src/model/entities/Carriage.java
+++ Client/src/model/entities/Carriage.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 public class Carriage
 {
     private int carriageId;
-    private final int seatCount = 16;
-    private ArrayList<Seat> seats;
+  private ArrayList<Seat> seats;
+  private ArrayList<Bicycle> bicycles;
 
     public Carriage(int carriageId) {
         this.carriageId = carriageId;
-        this.seats = new ArrayList<>(seatCount);
+      int seatCount = 16;
+      this.seats = new ArrayList<>(seatCount);
+      int bicycleCount = 2;
+      this.bicycles = new ArrayList<>(bicycleCount);
     }
     public int getCarriageId() {
         return carriageId;
@@ -34,7 +37,33 @@ public class Carriage
             }
         }
     }
-
+    public void addBicycle(Bicycle bicycle) {
+        this.bicycles.add(bicycle);
+    }
+    public ArrayList<Bicycle> getBicycles() {
+        return bicycles;
+    }
+    public void removeBicycle(Bicycle bicycle) {
+        this.bicycles.remove(bicycle);
+    }
+    public void removeBicycleById(int bicycleId) {
+        for (int i = 0; i < bicycles.size(); i++) {
+            if (bicycles.get(i).getBicycleId() == bicycleId) {
+                bicycles.remove(i);
+            }
+        }
+    }
+    public String toString() {
+      String str="";
+      str += "Carriage ID: " + carriageId + "\n";
+      for (int i = 0; i < seats.size(); i++) {
+          str += "Seat ID: " + seats.get(i).getSeatId() + ", Reserved: " + seats.get(i).isReserved() + "\n";
+      }
+      for (int i = 0; i < bicycles.size(); i++) {
+          str += "Bicycle ID: " + bicycles.get(i).getBicycleId() + ", Reserved: " + bicycles.get(i).isReserved() + "\n";
+      }
+      return str;
+    }
 
 
 }
