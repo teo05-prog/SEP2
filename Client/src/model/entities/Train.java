62cd696e126a0e45ccef56d3d0bc8698a164d552
bianca0203 <biancabuzdugan00@gmail.com>
2025-05-01 11:12:55 +0200

    Remove departure date and time attributes from Train class
    Took 56 seconds

--- Client/src/model/entities/Train.java
+++ Client/src/model/entities/Train.java
@@ -8,16 +8,12 @@ public class Train
   private ArrayList<Carriage> carriages;
   private Station from;
   private Station to;
-  private MyDate departureDate;
-  private MyDate departureTime;
 
   public Train(int trainId,ArrayList<Carriage> carriages, Station from, Station to, MyDate departureDate, MyDate departureTime) {
     this.trainId= trainId;
     this.carriages=new ArrayList<>();
     this.from=from;
     this.to=to;
-    this.departureDate=departureDate;
-    this.departureTime=departureTime;
   }
   public ArrayList<Carriage> getCarriages() {
     return carriages;
@@ -54,18 +50,7 @@ public void removeCarriageById(int carriageId) {
   public void setTo(Station to) {
     this.to = to;
   }
-  public MyDate getDepartureDate() {
-    return departureDate;
-  }
-  public void setDepartureDate(MyDate departureDate) {
-    this.departureDate = departureDate;
-  }
-  public MyDate getDepartureTime() {
-    return departureTime;
-  }
-  public void setDepartureTime(MyDate departureTime) {
-    this.departureTime = departureTime;
-  }
+
   public String toString()
   {
     String string = "";
