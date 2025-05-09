503c3bfd4da91e45d19953448c5598c6a80a6214
bianca0203 <biancabuzdugan00@gmail.com>
2025-05-09 18:59:07 +0200

    Add UpcomingDeparturesView and ViewModel for displaying upcoming departures
    Took 1 hour 12 minutes

---
+++ Client/src/view/traveller/upcomingDepartures/UpcomingDeparturesViewController.java
@@ -0,0 +1,93 @@
+package view.traveller.upcomingDepartures;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import model.entities.Ticket;
+import view.ViewHandler;
+import viewmodel.UpcomingDeparturesVM;
+
+public class UpcomingDeparturesViewController
+{
+  private UpcomingDeparturesVM viewModel;
+
+  @FXML private Button startButton;
+  @FXML private Button previousButton;
+  @FXML private Button upcomingButton;
+  @FXML private Button myAccountButton;
+  @FXML private ListView<Ticket> upcomingDeparturesListView;
+
+  public UpcomingDeparturesViewController()
+  {
+    this.viewModel = new UpcomingDeparturesVM();
+  }
+  public void init(UpcomingDeparturesVM viewModel)
+  {
+    if (viewModel != null)
+    {
+      this.viewModel = viewModel;
+    }
+    setupUI();
+    bindProperties();
+  }
+  @FXML public void initialize()
+  {
+    if (viewModel == null)
+    {
+      viewModel = new UpcomingDeparturesVM();
+    }
+  }
+  private void setupUI()
+  {
+    upcomingButton.setDisable(true);
+  }
+  public void bindProperties() {
+    // Bind the ListView to the upcoming departures
+    upcomingDeparturesListView.setItems(viewModel.getUpcomingDepartures());
+
+    // customize how tickets are displayed
+    //can be deleted if you want the departures to be displayed as default))
+    upcomingDeparturesListView.setCellFactory(param -> new ListCell<Ticket>() {
+      @Override
+      protected void updateItem(Ticket ticket, boolean empty) {
+        super.updateItem(ticket, empty);
+
+        if (empty || ticket == null) {
+          setText(null);
+        } else {
+          setText(ticket.getTrainId() + " - " +
+              "Departure: " + ticket.getDepartureTime().toString() +
+              " Arrival: " + ticket.getArrivalTime().toString());
+        }
+      }
+    });
+  }
+
+  public void onStartButton(ActionEvent e)
+  {
+    if (e.getSource() == startButton)
+    {
+      ViewHandler.showView(ViewHandler.ViewType.SEARCH_TICKET);
+    }
+  }
+
+  public void onPreviousButton(ActionEvent e)
+  {
+    //implement later
+  }
+
+  public void onUpcomingButton()
+  {
+    // do nothing because we are already on Upcoming departures page
+  }
+
+  public void onMyAccountButton(ActionEvent e)
+  {
+    if (e.getSource() == myAccountButton)
+    {
+      ViewHandler.showView(ViewHandler.ViewType.USER_ACCOUNT);
+    }
+  }
+}
---
+++ Client/src/viewmodel/UpcomingDeparturesVM.java
@@ -0,0 +1,12 @@
+package viewmodel;
+
+import javafx.collections.ObservableList;
+import model.entities.Ticket;
+
+public class UpcomingDeparturesVM
+{
+  public ObservableList<Ticket> getUpcomingDepartures()
+  {
+    //joih
+  }
+}
