7ce27683658e4cd74a5e0ddb172bbfc2ec51a3e9
bianca0203 <biancabuzdugan00@gmail.com>
2025-05-14 20:47:01 +0200

    Add PreviousDeparturesController and FXML view for previous departures
    Took 1 minute

---
+++ Client/src/viewmodel/PreviousDeparturesVM.java
@@ -0,0 +1,12 @@
+package viewmodel;
+
+import javafx.collections.ObservableList;
+import model.entities.Ticket;
+
+public class PreviousDeparturesVM
+{
+  public ObservableList<Ticket> getPreviousDepartures()
+  {
+
+  }
+}
