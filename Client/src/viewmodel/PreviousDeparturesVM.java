daf0dd9b69d339be54f0569d92bd41b7ee1e18b9
bianca0203 <biancabuzdugan00@gmail.com>
2025-05-15 20:01:30 +0200

    Implement PreviousDeparturesVM with data loading and refresh functionality
    Took 6 minutes

--- Client/src/viewmodel/PreviousDeparturesVM.java
+++ Client/src/viewmodel/PreviousDeparturesVM.java
@@ -1,12 +1,32 @@
 package viewmodel;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.entities.Ticket;
 
 public class PreviousDeparturesVM
 {
+  private ObservableList<Ticket> previousDepartures;
+
+  public PreviousDeparturesVM()
+  {
+    previousDepartures = FXCollections.observableArrayList();
+    loadPreviousDepartures();
+  }
+
+  private void loadPreviousDepartures()
+  {
+    // empty list for now
+
+  }
+
   public ObservableList<Ticket> getPreviousDepartures()
   {
+    return previousDepartures;
+  }
 
+  // refresh data when needed
+  public void refreshData() {
+    loadPreviousDepartures();
   }
 }
